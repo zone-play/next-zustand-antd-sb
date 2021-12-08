8724e4d16b545b655d70d3b7a17d19ff3bb7ab8a
DanaCoding <modana@protonmail.com>
2021-12-09 00:35:37 +0800

    项目初始化

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import 'antd/dist/antd.css';
+import * as NextImage from "next/image";
+const OriginalNextImage = NextImage.default;
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+    />
+  ),
+});
